efdd7451f39cd5f4427eb1a55a40243cae780d59
Alisan Cavus <alisan.cavus@linkus.eu>
2023-04-03 01:09:00 +0200

    electron setup done

---
+++ desktop/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className=' bg-orange-300 h-screen w-screen'>
+      Hello mofos!
+    </div>
+  )
+}
+
+export default App
